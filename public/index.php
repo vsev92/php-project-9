36166a60ed2380537c2489791bd68fd220e0f624
vsev92 <vsev92@gmail.com>
2025-01-23 16:02:22 +0700

    after recommit2

--- public/index.php
+++ public/index.php
@@ -33,20 +33,18 @@ $container->set(DIConfigurator::class, function () {
 
 
     return new DIConfigurator();
-
 });
 
 $container->set(DbConnector::class, function (DIConfigurator $dic) {
 
     $url = $dic->getUrlForDbConnector();
     return new DbConnector($url);
-
 });
 
 
 $container->set(SiteDAO::class, function (DbConnector $dbc) {
 
-    
+
     return new SiteDAO($dbc->getConnection());
 });
 
@@ -57,7 +55,7 @@ $container->set(CheckDAO::class, function (DbConnector $dbc) {
 
 
 $container->set('renderer', function () {
-   
+
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 });
 
@@ -76,8 +74,8 @@ $router = $app->getRouteCollector()->getRouteParser();
 
 
 $app->get('/', function ($request, $response, $args) {
-	
-	$params = ['inputValid' => true];
+
+    $params = ['inputValid' => true];
     return $this->get('renderer')->render($response, '/../templates/index.phtml', $params);
 })->setName('home');
 
@@ -87,7 +85,7 @@ $app->get('/urls', function ($request, $response, $args) {
     $messageType = array_keys($messages)[0];
     $message = $messages[$messageType][0];
 
-  
+
     $siteDAO = $this->get(SiteDAO::class);
     $sites = $siteDAO->getAll();
 
@@ -98,25 +96,25 @@ $app->get('/urls', function ($request, $response, $args) {
 
 
 $app->get('/urls/{id}', function ($request, $response, $args) {
-  
+
 
     $id = $args['id'];
     $siteDAO = $this->get(SiteDAO::class);
     $site = $siteDAO->findById($id);
-    if(!isset($site)) {
+    if (!isset($site)) {
         $newResponce = $response->withStatus(404);
         return $this->get('renderer')->render($newResponce, '/../templates/PageNotFound.phtml');
-    } 
- 
+    }
+
     $checkDAO = $this->get(CheckDAO::class);
     $checks = $checkDAO->findChecksBySiteId($id);
 
     $messages = $this->get('flash')->getMessages();
     $messageType = array_keys($messages)[0];
     $message = $messages[$messageType][0];
-    $params = ['site' => $site, 'checks'=> $checks, 'flash' => $message, 'flashType' => $messageType];
+    $params = ['site' => $site, 'checks' => $checks, 'flash' => $message, 'flashType' => $messageType];
 
-    
+
 
     return $this->get('renderer')->render($response, '/../templates/url.phtml', $params);
 })->setName('url');
@@ -124,21 +122,20 @@ $app->get('/urls/{id}', function ($request, $response, $args) {
 $app->post('/urls', function ($request, $response) use ($router) {
 
 
-   
 
-	$aUrl= $request->getParsedBody()['url'];
+
+    $aUrl = $request->getParsedBody()['url'];
     $urlRaw = $aUrl['name'];
 
-    
-    if(Site::isUrlValid($urlRaw)) {
+
+    if (Site::isUrlValid($urlRaw)) {
         $site = new Site($urlRaw);
         $siteDAO = $this->get(SiteDAO::class);
         $siteFromDB = $siteDAO->findByName($site->getUrl());
-        if(is_null($siteFromDB)){
-            if($siteDAO->save($site)) {
+        if (is_null($siteFromDB)) {
+            if ($siteDAO->save($site)) {
                 $this->get('flash')->addMessage('success', 'Страница успешно добавлена');
                 $id = $site->getId();
-    
             } else {
 
                 return $this->get('renderer')->render($response, '/../templates/ServerError.phtml');
@@ -148,20 +145,16 @@ $app->post('/urls', function ($request, $response) use ($router) {
             $id = $siteFromDB->getId();
         }
 
-   
+
         $url = $router->urlFor('url', ['id' => $id]);
         $newResponce = $response->withRedirect($url);
         return $newResponce;
-
     } else {
 
         $params = ['inputValidation' => false, 'url' => $urlRaw];
         $newResponce = $response->withStatus(422);
         return $this->get('renderer')->render($newResponce, '/../templates/index.phtml', $params);
-
     }
-   
-    
 });
 
 $app->post('/urls/{url_id}/checks', function ($request, $response, $args) use ($router) {
@@ -169,7 +162,7 @@ $app->post('/urls/{url_id}/checks', function ($request, $response, $args) use ($
     $check =  new Check($id);
     $siteDAO = $this->get(SiteDAO::class);
     $site = $siteDAO->findById((string)$id);
-    try{
+    try {
         $check->check($site->getUrl());
         $checkDAO = $this->get(CheckDAO::class);
         $checkDAO->save($check);
@@ -178,15 +171,11 @@ $app->post('/urls/{url_id}/checks', function ($request, $response, $args) use ($
         $this->get('flash')->addMessage('danger', 'Произошла ошибка при проверке, не удалось подключиться');
     } catch (ClientException $e) {
         return $this->get('renderer')->render($response, '/../templates/ServerError.phtml');
-    } 
+    }
     $url = $router->urlFor('url', ['id' => $id]);
     $newResponce = $response->withRedirect($url);
     return $newResponce;
-
 });
 
 
 $app->run();
-
-
-
